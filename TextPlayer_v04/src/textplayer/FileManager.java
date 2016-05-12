1d381dffc31ce463f365067ff9285ed055f88927
gmhertz <gunter.mhertz@gmail.com>
2016-05-12 18:02:32 -0300

    Interface pronta

---
+++ TextPlayer_v04/src/textplayer/FileManager.java
@@ -0,0 +1,105 @@
+package textplayer;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.*;
+
+/**
+ *
+ * @author gnmartins
+ */
+public class FileManager {
+
+    private static File handler;
+    
+    public static List<String> loadFromFile()
+    {
+        JFileChooser chooser = new JFileChooser();
+        //Setting to choose only from .txt files
+        FileFilter f = chooser.getFileFilter();
+        chooser.removeChoosableFileFilter(f);
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files (*.txt)", "txt", "text");
+        chooser.setFileFilter(filter);
+        
+        int chosen = chooser.showOpenDialog(null);
+        if (chosen == JFileChooser.APPROVE_OPTION)
+        {
+            handler = chooser.getSelectedFile();
+            return load();
+        }    
+        return null;
+    }
+    
+    public static void saveToFile(String content)
+    {
+        JFileChooser chooser = new JFileChooser();
+        int chosen = chooser.showSaveDialog(null);
+        if (chosen == JFileChooser.APPROVE_OPTION)
+        {
+            handler = chooser.getSelectedFile();
+            //If file with selected name doesn't already exists, 
+            //appends '.txt' to the selected file name.
+            if (!handler.exists()) 
+                handler = new File(chooser.getSelectedFile() + ".txt");
+            boolean success = save(content);
+            if (success)
+            {
+                JOptionPane.showMessageDialog(null,
+                        "File saved.");
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null,
+                        "Error saving file.",
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        }    
+    }
+    
+    private static List<String> load() 
+    {
+        List<String> text = new ArrayList<String>();
+        String line;
+        try (BufferedReader reader = new BufferedReader(new FileReader(handler))) 
+        {
+            while ((line = reader.readLine()) != null)
+                text.add(line);
+            reader.close();
+            return text;
+        }
+        catch (Exception e)
+        {
+            JOptionPane.showMessageDialog(null,
+                    "Error trying to read file '" + handler.getName() + "'.",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE);
+            return null;
+        }
+    }
+    
+    private static boolean save(String content)
+    {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(handler)))
+        {
+            //Sets the breaklines to default from system
+            content = content.replace("\n", System.getProperty("line.separator"));
+            writer.write(content);
+            writer.close();
+            return true;
+        }
+        catch (Exception e)
+        {
+            System.err.format("Error trying to write to '%s'.", handler.getName());
+            return false;
+        }
+    }
+          
+}
